1cb82e58a0ef57ef0284ff978e43b9a0edd30d02
Russ Perry <rperry1886@gmail.com>
2019-04-03 18:05:39 -0400

    Add Image Link Form, Style Image Link Form

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import Navigation from "./components/Navigation/Navigation";
 import Logo from "./components/Logo/Logo";
+import ImageLinkForm from "./components/ImageLinkForm/ImageLinkForm";
 import "./App.css";
 
 class App extends Component {
@@ -9,9 +10,8 @@ class App extends Component {
       <div className="App">
         <Navigation />
         <Logo />
-        {/* 
         <ImageLinkForm />
-        <FaceRecognition /> */}
+        {/* <FaceRecognition /> */}
       </div>
     );
   }
